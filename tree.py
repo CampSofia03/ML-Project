04aee5c3808de920366a5503d2c2cde61b62b85e
CampSofia03 <campreghersofia@gmail.com>
2025-04-19 10:27:22 +0200

    Update tree.py

--- tree.py
+++ tree.py
@@ -1,23 +1,25 @@
 import math
+import pandas as pd
 
+# A small epsilon to avoid log(0)
 eps = 1e-10
 
-
+# Gini Impurity function
 def gini_imp(p):
     return 1 - (p**2 + (1 - p) ** 2)
 
-
+# Entropy function
 def entropy(p):
     return -(p * math.log(p + eps) + (1 - p) * math.log(1 - p + eps))
 
-
+# Node class
 class Node:
     def __init__(self, dx=None, sx=None, dec_fn=None):
         self.dx = dx
         self.sx = sx
-        self.dec_fn = dec_fn
+        self.dec_fn = dec_fn  
 
-        self.is_leaf = self._update_leaf_status()  # creates / updates the leaf status
+        self.is_leaf = self._update_leaf_status() 
         self.target = None
 
     def _update_leaf_status(self):
@@ -27,38 +29,42 @@ class Node:
         self.dx = node_dx
         self.is_leaf = self._update_leaf_status()
 
-    def add_sx(self, node_dx):
-        self.sx = node_dx
+    def add_sx(self, node_sx):
+        self.sx = node_sx
         self.is_leaf = self._update_leaf_status()
 
     def expand_node(self, dec_fn):
         self.dec_fn = dec_fn
-
         self.add_sx(Node())
         self.add_dx(Node())
 
-
+# Binary Tree Predictor
 class BinaryTreePredictor:
     def __init__(self, dec_fn=None, split_fn=None, stop_fn=None):
-        self.dec_fn = dec_fn
-        self.split_fn = split_fn
-        self.stop_fn = stop_fn
+        self.dec_fn = dec_fn  # Decision function
+        self.split_fn = split_fn  # Function for splitting data
+        self.stop_fn = stop_fn  # Stopping function for tree expansion
 
     def fit(self, X, y):
         pass
 
-
 def in_order(node):
-    it = node
-    if it is None:
+    if node is None:
         return
 
-    in_order(it.sx)
-    print(it.target)
-    in_order(it.dx)
+    in_order(node.sx)
+    print(node.target)
+    in_order(node.dx)
 
-
+# Main function
 if __name__ == "__main__":
-    # print(gini_imp(0.5))
 
-    print(entropy(0))
+    dataset_dict = fetch_dataset()
+
+    # Entropy
+    print(f"Entropy at p=0: {entropy(0)}")
+    print(f"Entropy at p=0.5: {entropy(0.5)}")
+    print(f"Entropy at p=1: {entropy(1)}")
+
+    # Gini Impurity - p=0.5
+    print(f"Gini Impurity at p=0.5: {gini_imp(0.5)}")
