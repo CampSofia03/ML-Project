b7eadf0b36619c9d1d3ee0ff9b720db5a62c0d36
CampSofia03 <campreghersofia@gmail.com>
2025-05-24 08:30:07 +0200

    Update general_info.py

--- general_info.py
+++ general_info.py
@@ -13,25 +13,19 @@ print(dataset_dict["variables"])
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Measures
 total_missing = dataset_dict["X"].isnull().sum().sum()
 missing_per_column = dataset_dict["X"].isnull().sum()
 missing_percentage = dataset_dict["X"].isnull().mean() * 100
 
-# Dataframe
 missing_data = pd.DataFrame({
     'Missing Values': missing_per_column,
     'Missing Percentage': missing_percentage
 })
 
-# Filter
 missing_data = missing_data[missing_data['Missing Values'] > 0]
 
-# Table
 print(missing_data)
 
-
-# Bar Plot
 plt.figure(figsize=(10, 6))
 missing_data['Missing Percentage'].sort_values().plot(kind='barh', color='skyblue')
 plt.title('Percentage of Missing Values per Column')
@@ -43,37 +37,34 @@ plt.show()
 # Handle missing values
 # ------------------------------
 
-# Remove columns with more than 50% missing values
+# 1. Remove columns with more than 50% missing values
 threshold = 50
 columns_to_drop = missing_data[missing_data['Missing Percentage'] > threshold].index
 dataset_dict_cleaned = dataset_dict["X"].drop(columns=columns_to_drop)
 
 print(f"\nColumns removed: {columns_to_drop.tolist()}")
 
-# Impute remaining missing values with mode
+# 2. Impute remaining missing values with mode
 for column in dataset_dict_cleaned.columns:
     if dataset_dict_cleaned[column].isnull().sum() > 0:
-        # Impute missing values with mode
-        mode_value = dataset_dict_cleaned[column].mode()[0]
+        mode_value = dataset_dict_cleaned[column].mode()[0] 
         dataset_dict_cleaned[column].fillna(mode_value, inplace=True)
         print(f"Imputed missing values in column: {column} with mode value: {mode_value}")
 
-
-# Missing values test
 total_missing_after = dataset_dict_cleaned.isnull().sum().sum()
-
 print(f"\nNumber of total missing values: {total_missing_after}")
-
 dataset_dict["X"] = dataset_dict_cleaned
 
 # ------------------------
 # Statistical Analysis - numeric columns
 # ------------------------
 
-print("Statistical Analysis for numeric columns:\n")
+
+print("Statistical Analysis for numeric columns:\n") 
+dataset_dict["X"] = dataset_dict["X"].drop(columns="Unnamed: 0", errors="ignore")
 print(dataset_dict["X"].describe().round(1))
 
-
+	
 # ------------------------
 # Distribution of the categorical variables
 # ------------------------
@@ -89,20 +80,18 @@ n_cols = 3
 n_rows = math.ceil(len(categorical_cols) / n_cols)
 
 fig, axes = plt.subplots(n_rows, n_cols, figsize=(18, 4 * n_rows))
-axes = axes.flatten()  # Rende l'array 1D per iterarci facilmente
+axes = axes.flatten()
 
 for i, col in enumerate(categorical_cols):
     sns.countplot(data=dataset_dict["X"], x=col, order=dataset_dict["X"][col].value_counts().index, ax=axes[i])
-    axes[i].set_title(f'Distribuzione: {col}')
-    axes[i].tick_params(axis='x', rotation=45)
+    axes[i].set_title(f'Distribution: {col}')
+    axes[i].tick_params(axis='x')
 
 for j in range(i + 1, len(axes)):
     fig.delaxes(axes[j])
 
 plt.tight_layout()
 plt.show()
-
-
 
 # ------------------------
 # Distribution of the numeric variables
@@ -121,10 +110,9 @@ for ax, col in zip(axes, numeric_cols):
     ax.set_xlabel('')
     ax.set_ylabel('')
 
-plt.suptitle('Distribuzione delle variabili numeriche', fontsize=16)
+plt.suptitle('Distribution of numeric variables', fontsize=16)
 plt.tight_layout()
 plt.show()
-
 
 # ------------------------
 # Correlation Matrix - numeric columns
@@ -135,7 +123,6 @@ correlation_matrix = dataset_dict["X"].corr()
 print("Correlation Matrix:\n")
 print(correlation_matrix)
 
-# CM - Heatmap:
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -144,24 +131,20 @@ sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", fmt=".2f")
 plt.title("Correlation Heatmap")
 plt.show()
 
-
 # ------------------------
-# Correlation Matrix 
+# Correlation Matrix - converting categorical into numeric value
 # ------------------------
 
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Encoding categorical into numeric value
 df_encoded = dataset_dict["X"].apply(pd.Categorical).apply(lambda x: x.cat.codes)
-df_encoded['target'] = dataset_dict["y"].apply(pd.Categorical).apply(lambda x: x.cat.codes)
+df_encoded['target'] = dataset_dict["y"]['class'].astype('category').cat.codes
 
-# Correlation Matrix
 corr_matrix = df_encoded.corr()
 
-# Heatmap plot 
 plt.figure(figsize=(12, 10))
 sns.heatmap(corr_matrix, cmap='coolwarm', fmt='.2f', cbar_kws={'label': 'correlation'})
-plt.title('Matrice di Correlazione delle Variabili di Input')
+plt.title('Correlation Matrix')
 plt.show()
